f84ce43fa7647dd97a0dbb1c86d9e42a647d7c95
chillax-C <1921754372@qq.com>
2026-07-21 16:22:07 +0800

    Site updated: 2026-07-21 16:21:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/18/hello-world/"];function toRandomPost(){
+var posts=["2026/07/18/搭建博客/","2026/07/21/LCG/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
